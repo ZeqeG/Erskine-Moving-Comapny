45cfadb5bd60a92f269e61f482bfbc13639dfe38
Jacklynn-Yan <jyanmoore@gmail.com>
2025-06-12 23:46:26 -0700

    added full js calc

--- js/getAPI.js
+++ js/getAPI.js
@@ -1,10 +1,15 @@
 var origin1 = "";
 var destination1 = "";
 function getQuote() {
-  origin1 = document.getElementById("quoteStartLocation").value.formattedAddress;
-  destination1 = document.getElementById("quoteEndLocation").value.formattedAddress;
-  console.log(origin1 + ', ' + destination1);
-  initMap();
+  if (document.getElementById("quoteStartLocation").value !== undefined && document.getElementById("quoteEndLocation").value !== undefined) {
+    console.log(document.getElementById("quoteStartLocation").value)
+    origin1 = document.getElementById("quoteStartLocation").value.formattedAddress;
+    destination1 = document.getElementById("quoteEndLocation").value.formattedAddress;
+    console.log(origin1 + ', ' + destination1);
+    initMap();
+  } else {
+    console.log('no input');
+  }
 }
 function initMap() {
   const bounds = new google.maps.LatLngBounds();
@@ -44,6 +49,46 @@ function initMap() {
     // show on map
     console.log(response.rows[0].elements[0].distance);
     console.log(response.rows[0].elements[0].duration);
+
+    const truckType = document.getElementById("truck-selector").value;
+    const distanceEl = document.getElementById("distance");
+    const timeEl = document.getElementById("time");
+    const costEl = document.getElementById("cost");
+    var truckCost = 0;
+    var truckTime = 0;
+    console.log(truckType);
+    switch (truckType) {
+      case '1':
+        truckCost = 11300;
+        truckTime = 180;
+        console.log('truck 1 ' + truckCost + ' ' + truckTime);
+        break;
+      case '2':
+        truckCost = 11900;
+        truckTime = 270;
+        console.log('truck 2 ' + truckCost + ' ' + truckTime);
+        break;
+      case '3':
+        truckCost = 14800;
+        truckTime = 360;
+        console.log('truck 3 ' + truckCost + ' ' + truckTime);
+        break;
+      case '4':
+        truckCost = 17800;
+        truckTime = 540;
+        console.log('truck 4 ' + truckCost + ' ' + truckTime);
+        break;
+    }
+    let timeMinutes = response.rows[0].elements[0].duration.value / 60 + truckTime
+    timeEl.innerText = Math.floor(timeMinutes / 60) + 'h' + Math.floor(timeMinutes % 60) + 'm';
+    distanceEl.innerText = response.rows[0].elements[0].distance.text;
+    console.log('truckcost' + truckCost);
+    console.log('timecost' + (150 * (truckTime + response.rows[0].elements[0].duration.value / 60)));
+    console.log('distancecost' + (0.8 * response.rows[0].elements[0].distance.value * 0.06213712));
+    let cost = Math.floor(Math.ceil((10000 + truckCost + (150 * (truckTime + response.rows[0].elements[0].duration.value / 60)) + (0.8 * response.rows[0].elements[0].distance.value * 0.06213712)) / 500) * 500);
+    console.log(cost);
+    costEl.innerText = '$' + (cost / 100).toLocaleString('en-US');
+
     const originList = response.originAddresses;
     const destinationList = response.destinationAddresses;
 
