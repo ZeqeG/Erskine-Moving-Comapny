5602694f8241ebba9b7c2a04c06e18cf9bf58bcd
zeqe <zeqe.g@icloud.com>
2025-06-13 22:05:03 -0700

    designin

--- js/sendFormData.js
+++ js/sendFormData.js
@@ -1,9 +1,16 @@
 window.addEventListener("load", function () {
     const form = document.getElementById('servicesSection4');
+    const submitBtn = document.getElementById('submitBtn');
     form.addEventListener("submit", function (e) {
+        if (submitBtn) {
+            submitBtn.value = 'Loading...'; // Use only textContent
+        }
         e.preventDefault();
         const data = new FormData(form);
         const action = e.target.action;
+
+
+
         fetch(action, {
             method: 'POST',
             body: data,
